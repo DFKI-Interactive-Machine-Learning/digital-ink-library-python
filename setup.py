57764ce27ddb60f2d8e884b89a8fe23404bc52dc
Michael Barz <mb@barz.de>
2020-05-29 10:34:09 +0200

    minor changes to setup scripts

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from distutils.core import setup
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name='digital_ink_library',
     version='1.4.1',
     packages=['digital_ink_library', 'digital_ink_library.serialization', 'digital_ink_library.conversion'],
     url='https://github.com/DFKI-Interactive-Machine-Learning/digital-ink-library-python',
     license='CC BY-NC-SA 4.0',
+    keywords=['digital pen'],
     author='Alexander Prange, Michael Barz',
     author_email='alexander.prange@dfki.de; michael.barz@dfki.de',
     python_requires='>=3.6.0',
@@ -17,6 +15,5 @@ setup(
             'numpy',
             'scipy'
         ],
-    description='Library for digital ink, written in Python.',
-    long_description=long_description
+    description='Library for digital ink, written in Python.'
 )
