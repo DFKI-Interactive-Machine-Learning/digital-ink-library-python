d5e3b57661b1b9d6d88a2ed9b333edba4fd47c4a
Michael Barz <mb@barz.de>
2020-05-29 12:51:36 +0200

    updated setup script (due to pypo upload)

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ setup(
     license='CC BY-NC-SA 4.0',
     keywords=['digital pen'],
     author='Alexander Prange, Michael Barz',
-    author_email='alexander.prange@dfki.de; michael.barz@dfki.de',
+    author_email='alexander.prange@dfki.de, michael.barz@dfki.de',
     python_requires='>=3.6.0',
+    download_url="https://github.com/DFKI-Interactive-Machine-Learning/digital-ink-library-python/releases/download/v1.4.1/digital_ink_library-1.4.1-py3-none-any.whl",
     install_requires=[
             'numpy',
             'scipy'
